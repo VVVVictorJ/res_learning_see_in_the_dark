9e7d408479c515dac5257616dbb2cae2a024a6ef
VVVVictorJ <956951190@qq.com>
2020-01-16 17:20:57 +0800

    some-modify-regrad-train-test

--- test_cpu.py
+++ test_cpu.py
@@ -13,6 +13,10 @@ input_dir = 'H:/sony/Sony/Sony/short/'
 gt_dir = 'H:/sony/Sony_gt_16bitPNG/gt/'
 checkpoint_dir = './result/'
 result_dir= './result/'
+# get test IDs
+test_fns = glob.glob(gt_dir + '/1*.png')
+test_ids = [int(os.path.basename(test_fn)[0:5]) for test_fn in test_fns]
+
 start = time.time()
 sess = tf.Session()
 in_image = tf.placeholder(tf.float32, [None, None, None, 4])
@@ -29,19 +33,48 @@ if ckpt:
 if not os.path.isdir(result_dir + 'final/'):
     os.makedirs(result_dir + 'final/')
 
-in_path = 'D:/test/iphone/7p/frame_3/IMG_6395.DNG'
+for test_id in test_ids:
+    # test the first image in each sequence
+    in_files = glob.glob(input_dir + '%05d_00*.ARW' % test_id)
+    for k in range(len(in_files)):
+        if k<10:
+            in_path = in_files[k]
+            in_fn = os.path.basename(in_path)
+            print(in_fn)
+            gt_files = glob.glob(gt_dir + '%05d_00*.png' % test_id)
+            gt_path = gt_files[0]
+            gt_fn = os.path.basename(gt_path)
+            in_exposure = float(in_fn[9:-5])
+            gt_exposure = float(gt_fn[9:-5])
+            ratio = min(gt_exposure / in_exposure, 300)
 
-in_fn = os.path.basename(in_path)
-ratio = 100
+            raw = rawpy.imread(in_path)
+            input_full = np.expand_dims(pack_raw(raw), axis=0) * ratio
 
-raw = rawpy.imread(in_path)
-input_full = np.expand_dims(pack_raw(raw), axis=0) * ratio
-im = raw.postprocess(use_camera_wb=True, half_size=False, no_auto_bright=True, output_bps=16)
-input_full = np.minimum(input_full, 1.0)
+            im = raw.postprocess(use_camera_wb=True, half_size=False, no_auto_bright=True, output_bps=16)
+            # scale_full = np.expand_dims(np.float32(im/65535.0),axis = 0)*ratio
+            scale_full = np.expand_dims(np.float32(im / 65535.0), axis=0)
 
-output = sess.run(out_image, feed_dict={in_image: input_full})
-output = np.minimum(np.maximum(output, 0), 1)
+            # gt_raw = rawpy.imread(gt_path)
+            #im = gt_raw.postprocess(use_camera_wb=True, half_size=False, no_auto_bright=True, output_bps=16)
+            im = Image.open(gt_path)
+            im = np.asarray(im)
+            gt_full = np.expand_dims(np.float32(im / 65535.0), axis=0)
 
-output = output[0, :, :, :]
-Image.fromarray((output*255).astype(np.uint8)).save(result_dir + 'final/10_r_%d_out.png' %  ratio)
-print(time.time()-start)
+            input_full = np.minimum(input_full, 1.0)
+
+            output = sess.run(out_image, feed_dict={in_image: input_full})
+            output = np.minimum(np.maximum(output, 0), 1)
+
+            output = output[0, :, :, :]
+            gt_full = gt_full[0, :, :, :]
+            scale_full = scale_full[0, :, :, :]
+            scale_full = scale_full * np.mean(gt_full) / np.mean(
+                scale_full)  # scale the low-light image to the same mean of the groundtruth
+
+            scipy.misc.toimage(output * 255, high=255, low=0, cmin=0, cmax=255).save(
+                result_dir + 'final/%5d_00_%d_out.png' % (test_id, ratio))
+            scipy.misc.toimage(scale_full * 255, high=255, low=0, cmin=0, cmax=255).save(
+                result_dir + 'final/%5d_00_%d_scale.png' % (test_id, ratio))
+            scipy.misc.toimage(gt_full * 255, high=255, low=0, cmin=0, cmax=255).save(
+                result_dir + 'final/%5d_00_%d_gt.png' % (test_id, ratio))
--- train_cpu.py
+++ train_cpu.py
@@ -1,4 +1,5 @@
 import os,time,scipy.io
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
 import numpy as np
 import rawpy
@@ -17,9 +18,9 @@ result_dir= './result/'
 
 
 # get train IDs
-train_fns = glob.glob(gt_dir + '0*.ARW')
+train_fns = glob.glob(gt_dir + '0*.png')
 train_ids = [int(os.path.basename(train_fn)[0:5]) for train_fn in train_fns]
-
+print(train_ids)
 ps = 512  # patch size for training
 save_freq = 2
 
@@ -31,7 +32,7 @@ in_image = tf.placeholder(tf.float32,[None,None,None,4])
 gt_image = tf.placeholder(tf.float32,[None,None,None,3])
 out_image = res_unet(in_image)
 
-G_Loss = tf.reduce_mean(abs(out_image-gt_image))
+G_Loss = tf.reduce_mean(tf.abs(out_image-gt_image))
 
 t_vars = tf.trainable_variables()
 
@@ -42,6 +43,8 @@ G_opt = tf.train.AdamOptimizer(learning_rate=lr).minimize(G_Loss)
 saver = tf.train.Saver()
 
 sess.run(tf.global_variables_initializer())
+
+print("before ckpt")
 
 ckpt = tf.train.get_checkpoint_state(checkpoint_dir)
 
@@ -69,68 +72,69 @@ for epoch in range(lastepoch, 4001):
     cnt = 0
     if epoch > 2000:
         learning_rate = 1e-5
+    print("start epoch")
 
     for ind in np.random.permutation(len(train_ids)):
         # get the path from image id
-        train_id = train_ids[ind]
-        print(train_id)
-        in_files = glob.glob(input_dir + '%05d_00*.ARW' % train_id)
-        in_path = in_files[np.random.random_integers(0, len(in_files) - 1)]
-        in_fn = os.path.basename(in_path)
 
-        gt_files = glob.glob(gt_dir + '%05d_00*.png' % train_id)
-        gt_path = gt_files[0]
-        gt_fn = os.path.basename(gt_path)
-        in_exposure = float(in_fn[9:-5])
-        gt_exposure = float(gt_fn[9:-5])#groudtruth
-        ratio = min(gt_exposure / in_exposure, 300)
+            train_id = train_ids[ind]
+            in_files = glob.glob(input_dir + '%05d_00*.ARW' % train_id)
+            in_path = in_files[np.random.random_integers(0, len(in_files) - 1)]
+            in_fn = os.path.basename(in_path)
 
-        st = time.time()
-        cnt += 1
+            gt_files = glob.glob(gt_dir + '%05d_00*.png' % train_id)
+            gt_path = gt_files[0]
+            gt_fn = os.path.basename(gt_path)
+            in_exposure = float(in_fn[9:-5])
+            gt_exposure = float(gt_fn[9:-5])#groudtruth
+            ratio = min(gt_exposure / in_exposure, 300)
 
-        if input_images[str(ratio)[0:3]][ind] is None:
-            raw = rawpy.imread(in_path)
-            input_images[str(ratio)[0:3]][ind] = np.expand_dims(pack_raw(raw), axis=0) * ratio
-            im = Image.open(gt_files)
-            im = np.asarray(im)
-            gt_images[ind] = np.expand_dims(np.float32(im / 65535.0), axis=0)
+            st = time.time()
+            cnt += 1
 
-        # crop
-        H = input_images[str(ratio)[0:3]][ind].shape[1]
-        W = input_images[str(ratio)[0:3]][ind].shape[2]
+            if input_images[str(ratio)[0:3]][ind] is None:
+                raw = rawpy.imread(in_path)
+                input_images[str(ratio)[0:3]][ind] = np.expand_dims(pack_raw(raw), axis=0) * ratio
+                im = Image.open(gt_path)
+                im = np.asarray(im)
+                gt_images[ind] = np.expand_dims(np.float32(im / 65535.0), axis=0)
 
-        xx = np.random.randint(0, W - ps)
-        yy = np.random.randint(0, H - ps)
-        input_patch = input_images[str(ratio)[0:3]][ind][:, yy:yy + ps, xx:xx + ps, :]
-        gt_patch = gt_images[ind][:, yy * 2:yy * 2 + ps * 2, xx * 2:xx * 2 + ps * 2, :]
+            # crop
+            H = input_images[str(ratio)[0:3]][ind].shape[1]
+            W = input_images[str(ratio)[0:3]][ind].shape[2]
 
-        if np.random.randint(2, size=1)[0] == 1:  # random flip
-            input_patch = np.flip(input_patch, axis=1)
-            gt_patch = np.flip(gt_patch, axis=1)
-        if np.random.randint(2, size=1)[0] == 1:
-            input_patch = np.flip(input_patch, axis=2)
-            gt_patch = np.flip(gt_patch, axis=2)
-        if np.random.randint(2, size=1)[0] == 1:  # random transpose
-            input_patch = np.transpose(input_patch, (0, 2, 1, 3))
-            gt_patch = np.transpose(gt_patch, (0, 2, 1, 3))
+            xx = np.random.randint(0, W - ps)
+            yy = np.random.randint(0, H - ps)
+            input_patch = input_images[str(ratio)[0:3]][ind][:, yy:yy + ps, xx:xx + ps, :]
+            gt_patch = gt_images[ind][:, yy * 2:yy * 2 + ps * 2, xx * 2:xx * 2 + ps * 2, :]
 
-        input_patch = np.minimum(input_patch, 1.0)
+            if np.random.randint(2, size=1)[0] == 1:  # random flip
+                input_patch = np.flip(input_patch, axis=1)
+                gt_patch = np.flip(gt_patch, axis=1)
+            if np.random.randint(2, size=1)[0] == 1:
+                input_patch = np.flip(input_patch, axis=2)
+                gt_patch = np.flip(gt_patch, axis=2)
+            if np.random.randint(2, size=1)[0] == 1:  # random transpose
+                input_patch = np.transpose(input_patch, (0, 2, 1, 3))
+                gt_patch = np.transpose(gt_patch, (0, 2, 1, 3))
 
-        _, G_current, output = sess.run([G_opt, G_Loss, out_image],
-                                        feed_dict={in_image: input_patch, gt_image: gt_patch, lr: learning_rate})
-        output = np.minimum(np.maximum(output, 0), 1)
-        g_loss[ind] = G_current
+            input_patch = np.minimum(input_patch, 1.0)
 
-        print("%d %d Loss=%.3f Time=%.3f" % (epoch, cnt, np.mean(g_loss[np.where(g_loss)]), time.time() - st))
+            _, G_current, output = sess.run([G_opt, G_Loss, out_image],
+                                            feed_dict={in_image: input_patch, gt_image: gt_patch, lr: learning_rate})
+            output = np.minimum(np.maximum(output, 0), 1)
+            g_loss[ind] = G_current
 
-        if epoch % save_freq == 0:
-            if not os.path.isdir(result_dir + '%04d' % epoch):
-                os.makedirs(result_dir + '%04d' % epoch)
+            print("%d %d Loss=%.3f Time=%.3f" % (epoch, cnt, np.mean(g_loss[np.where(g_loss)]), time.time() - st))
 
-            temp = np.concatenate((gt_patch[0, :, :, :], output[0, :, :, :]), axis=1)
-            # scipy.misc.toimage(temp * 255, high=255, low=0, cmin=0, cmax=255).save(
-            #     result_dir + '%04d/%05d_00_train_%d.jpg' % (epoch, train_id, ratio))
-            Image.fromarray((output * 255).astype(np.uint8)).save(
-                result_dir + '%04d/%05d_00_train_%d.jpg' % (epoch, train_id, ratio))
+            if epoch % save_freq == 0:
+                if not os.path.isdir(result_dir + '%04d' % epoch):
+                    os.makedirs(result_dir + '%04d' % epoch)
+
+                temp = np.concatenate((gt_patch[0, :, :, :], output[0, :, :, :]), axis=1)
+                scipy.misc.toimage(temp * 255, high=255, low=0, cmin=0, cmax=255).save(
+                    result_dir + '%04d/%05d_00_train_%d.jpg' % (epoch, train_id, ratio))
+                # Image.fromarray((output * 255).astype(np.uint8)).save(
+                #     result_dir + '%04d/%05d_00_train_%d.jpg' % (epoch, train_id, ratio))
 
     saver.save(sess, checkpoint_dir + 'model.ckpt')
