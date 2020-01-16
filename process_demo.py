7543200c9e235e426db518d98597d4ca1abe8795
VVVVictorJ <956951190@qq.com>
2020-01-16 17:17:07 +0800

    process-file

---
+++ process_demo.py
@@ -0,0 +1,141 @@
+# uniform content loss + adaptive threshold + per_class_input + recursive G
+# improvement upon cqf37
+from __future__ import division
+import os, scipy.io,time
+import tensorflow as tf
+import tensorflow.contrib.slim as slim
+import numpy as np
+import rawpy
+import glob
+from PIL import Image
+
+
+input_dir = 'H:/sony/Sony/Sony/short/'
+gt_dir = 'H:/sony/Sony_gt_16bitPNG/gt/'
+checkpoint_dir = './checkpoint/Sony/'
+result_dir = './result_Sony/'
+
+DEBUG = 0
+if DEBUG == 1:
+    save_freq = 2
+    test_ids = test_ids[0:5]
+
+
+def lrelu(x):
+    return tf.maximum(x * 0.2, x)
+
+
+def upsample_and_concat(x1, x2, output_channels, in_channels):
+    pool_size = 2
+    deconv_filter = tf.Variable(tf.truncated_normal([pool_size, pool_size, output_channels, in_channels], stddev=0.02))
+    deconv = tf.nn.conv2d_transpose(x1, deconv_filter, tf.shape(x2), strides=[1, pool_size, pool_size, 1])
+
+    deconv_output = tf.concat([deconv, x2], 3)
+    deconv_output.set_shape([None, None, None, output_channels * 2])
+
+    return deconv_output
+
+
+def network(input):
+    conv1 = slim.conv2d(input, 32, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv1_1')
+    conv1 = slim.conv2d(conv1, 32, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv1_2')
+    pool1 = slim.max_pool2d(conv1, [2, 2], padding='SAME')
+
+    conv2 = slim.conv2d(pool1, 64, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv2_1')
+    conv2 = slim.conv2d(conv2, 64, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv2_2')
+    pool2 = slim.max_pool2d(conv2, [2, 2], padding='SAME')
+
+    conv3 = slim.conv2d(pool2, 128, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv3_1')
+    conv3 = slim.conv2d(conv3, 128, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv3_2')
+    pool3 = slim.max_pool2d(conv3, [2, 2], padding='SAME')
+
+    conv4 = slim.conv2d(pool3, 256, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv4_1')
+    conv4 = slim.conv2d(conv4, 256, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv4_2')
+    pool4 = slim.max_pool2d(conv4, [2, 2], padding='SAME')
+
+    conv5 = slim.conv2d(pool4, 512, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv5_1')
+    conv5 = slim.conv2d(conv5, 512, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv5_2')
+
+    up6 = upsample_and_concat(conv5, conv4, 256, 512)
+    conv6 = slim.conv2d(up6, 256, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv6_1')
+    conv6 = slim.conv2d(conv6, 256, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv6_2')
+
+    up7 = upsample_and_concat(conv6, conv3, 128, 256)
+    conv7 = slim.conv2d(up7, 128, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv7_1')
+    conv7 = slim.conv2d(conv7, 128, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv7_2')
+
+    up8 = upsample_and_concat(conv7, conv2, 64, 128)
+    conv8 = slim.conv2d(up8, 64, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv8_1')
+    conv8 = slim.conv2d(conv8, 64, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv8_2')
+
+    up9 = upsample_and_concat(conv8, conv1, 32, 64)
+    conv9 = slim.conv2d(up9, 32, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv9_1')
+    conv9 = slim.conv2d(conv9, 32, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv9_2')
+
+    conv10 = slim.conv2d(conv9, 12, [1, 1], rate=1, activation_fn=None, scope='g_conv10')
+    out = tf.depth_to_space(conv10, 2)
+    return out
+
+
+def pack_raw(raw):
+    # pack Bayer image to 4 channels
+    im = raw.raw_image_visible.astype(np.float32)
+    im = np.maximum(im - 512, 0) / (4095 - 512)  # subtract the black level
+
+    im = np.expand_dims(im, axis=2)
+    img_shape = im.shape
+    H = img_shape[0]
+    W = img_shape[1]
+
+    out = np.concatenate((im[0:H:2, 0:W:2, :],
+                          im[0:H:2, 1:W:2, :],
+                          im[1:H:2, 1:W:2, :],
+                          im[1:H:2, 0:W:2, :]), axis=2)
+    return out
+
+
+sess = tf.Session()
+in_image = tf.placeholder(tf.float32, [None, None, None, 4])
+gt_image = tf.placeholder(tf.float32, [None, None, None, 3])
+out_image = network(in_image)
+
+saver = tf.train.Saver()
+sess.run(tf.global_variables_initializer())
+ckpt = tf.train.get_checkpoint_state(checkpoint_dir)
+if ckpt:
+    print('loaded ' + ckpt.model_checkpoint_path)
+    saver.restore(sess, ckpt.model_checkpoint_path)
+
+if not os.path.isdir(result_dir + 'final_1/'):
+    os.makedirs(result_dir + 'final_1/')
+
+def process(filelist,ratio):
+    for file in filelist:
+        start_1 = time.time()
+        in_frame = os.path.basename(file)
+        print(in_frame)
+        raw = rawpy.imread(file)
+        ratio_1 = int(ratio)
+        in_full = np.expand_dims(pack_raw(raw),axis=0)*ratio_1
+        #im = raw.postprocess(use_camera_wb=True, half_size=False, no_auto_bright=True, output_bps=16)
+
+        in_full = np.minimum(in_full, 1.0)
+        output = sess.run(out_image,feed_dict={in_image:in_full})
+        output = np.minimum(np.maximum(output, 0), 1)
+        output = output[0, :, :, :]
+
+        scipy.misc.toimage(output * 255, high=255, low=0, cmin=0, cmax=255).save(
+            result_dir + 'final/%s_00_%d_out.png' % (in_frame, ratio_1))
+        print(time.time()-start_1)
+def main():
+    path = input("path is \t")
+    ratio = input("please input ratio what you want \t")
+    files = glob.glob(path+'*.DNG')
+    start = time.time()
+    process(files,ratio)
+    print(time.time()-start)
+if __name__ == '__main__':
+    main()
+
+#test = D:/test/iphone/7p/frame_3/
+
